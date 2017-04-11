b278b043e361f0f69e1a30e78cbff97e1027f0e7
Zack <zholloway92@gmail.com>
2017-04-11 13:48:04 -0400

    first commit. gift class created in model

---
+++ GiftExchange/Models/Gift.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiftExchange.Models
+{
+    public class Gift
+    {
+        public string Contents { get; set; }
+        public string GiftHint { get; set; }
+        public string ColorWrappingPaper { get; set; }
+        public double Height { get; set; }
+        public double Depth { get; set; }
+        public double Weight { get; set; }
+        public bool IsOpened { get; set; }
+
+        public Gift() { }
+    }
+}
